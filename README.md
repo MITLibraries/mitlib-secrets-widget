ed8d473e888cf4c4ddb872d17807b32652dabd24
Matt Bernhardt <mjbernha@mit.edu>
2019-01-08 10:10:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# MIT Libraries Secrets Widget
+
+[![Build Status](https://travis-ci.org/MITLibraries/mitlib-secrets-widget.svg)](https://travis-ci.org/MITLibraries/mitlib-secrets-widget)
+[![Code Climate](https://codeclimate.com/github/MITLibraries/mitlib-secrets-widget/badges/gpa.svg)](https://codeclimate.com/github/MITLibraries/mitlib-secrets-widget)
+[![Issue Count](https://codeclimate.com/github/MITLibraries/mitlib-secrets-widget/badges/issue_count.svg)](https://codeclimate.com/github/MITLibraries/mitlib-secrets-widget)
+
+This plugin provides an administrative widget that lists defined secrets that are available in that environment.
---
+++ mitlib-secrets-widget.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Plugin Name: MITlib Secrets Widget
+ * Plugin URI: https://github.com/MITLibraries/mitlib-secrets-widget
+ * Description: A plugin to list the defined secrets (keys, not their values) coming from Pantheon's Terminus Secrets plugin
+ * Version: 0.0.1
+ * Author: Matt Bernhardt
+ * Author URI: https://github.com/matt-bernhardt
+ * License: GPL2
+ *
+ * @package MITlib Secrets Widget
+ * @author Matt Bernhardt
+ * @link https://github.com/MITLibraries/mitlib-secrets-widget
+ */
+
+/**
+ * MITlib Secrets Widget is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * MITlib Secrets Widget is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MITlib Secrets Widget. If not, see {URI to Plugin License}.
+ */
+
+namespace mitlib;
+
+// Don't call the file directly!
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Include the necessary classes.
+// require_once 'class-mitlib-secrets.php';
+
+// Function that outputs the contents of the dashboard widget
+function secrets_widget_dashboard( $post, $callback_args ) {
+	echo "Hello World, this is my first Dashboard Widget!";
+}
+
+/**
+ * Registers base widget.
+ */
+function secrets_register_dashboard_widget() {
+	wp_add_dashboard_widget( 'mitlib\MITlib_Secrets', 'MITlib Secrets', 'mitlib\secrets_widget_dashboard');
+}
+add_action( 'wp_dashboard_setup', 'mitlib\secrets_register_dashboard_widget' );
